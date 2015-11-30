9e574c756e8d477eaf84dedf238c473bc9711b75
faabey33 <fmainziii@gmail.com>
2015-11-30 18:15:11 +0100

    support for custom background images and font

--- main.lua
+++ main.lua
@@ -5,22 +5,40 @@ require "textbox";
 
 function love.load()
 	textbox:init();
-	textbox:newBox(100,200,200,20,1,"");
 
-	textbox:newBox(400,200,200,20,1,"");
+	textbox:newBox(100,200,200,40,1,"Hello!",0,1,1,5,5);
+	textbox:newBox(400,200,220,180,1,"",1,0,20,32);
+	textbox:newBox(800,400,220,180,1,"",2,2,10,32);
 
+	textbox1 = love.graphics.newImage("textbox1.png")
+	textbox2 = love.graphics.newImage("textbox2.png")
+	background = love.graphics.newImage("back.png")
+
+	defaultfont = love.graphics.newFont(14)
+	classyfont = love.graphics.newFont("Dosmilcatorce.ttf", 24)
+	coolfont = love.graphics.newImageFont("imagefont.png",
+    " abcdefghijklmnopqrstuvwxyz" ..
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ0" ..
+    "123456789.,!?-+/():;%&`'*#=[]\"")
+
+
+
+    love.graphics.setFont(classyfont)
 
 end
 
 function love.update(dt)
 
 
-
 	textbox:update();
 
 
+end
 
-
+function love.keypressed(k)
+	if k == "backspace" then
+		textbox:delete()
+	end	
 end
 
 
@@ -29,40 +47,14 @@ end
 
 function love.draw()
 
+	love.graphics.draw(background, 0, 0)
+
 	textbox:draw();
+
 
 
 	love.graphics.print(textbox.boxCount, 0, 0);
 
 	love.graphics.print(textbox.selectedBox, 100, 100);
 
-end
-
-
---from https://love2d.org/wiki/String_extensions
-
-local meta = getmetatable("") -- get the string metatable
- 
-meta.__add = function(a,b) -- the + operator
-    return a..b
-end
- 
-meta.__sub = function(a,b) -- the - operator
-    return a:gsub(b,"")
-end
- 
-meta.__mul = function(a,b) -- the * operator
-    return a:rep(b)
-end
- 
--- if you have string.explode (check out the String exploding snippet) you can also add this:
-meta.__div = function(a,b) -- the / operator
-    return a:explode(b)
-end
- 
-meta.__index = function(a,b) -- if you attempt to do string[id]
-    if type(b) ~= "number" then
-        return string[b]
-    end
-    return a:sub(b,b)
 end
--- textbox.lua
+++ textbox.lua
@@ -9,12 +9,16 @@ function textbox:init()
 
 	textbox.boxCount = 0;
 	textbox.selectedBox = 0;
+	textbox.boxhp = {}
+	textbox.boxvp = {}
+	textbox.font = {}
 
 	textbox.text = {}
+	textbox.style = {}
 
 
 
-	mosuex = 0
+	mosuex = 0;
 	mousey = 0;
 
 
@@ -31,22 +35,24 @@ function textbox:update()
 		mousex, mousey = love.mouse.getPosition();
 		
 		if (mousex>self.boxX[a] and mousex<self.boxX[a]+self.boxW[a] and mousey>self.boxY[a] and mousey<self.boxY[a]+self.boxH[a] and love.mouse.isDown("l")) then
+			self.drag=true;
 			self.selectedBox = a;
 		end
+
 		a=a+1;
 	end
 
-	if love.keyboard.isDown("backspace") then
-		
-		local string = textbox.text[textbox.selectedBox]
-
-		string = string:sub( 1, #string - 1 )
-
-		textbox.text[textbox.selectedBox] = string;
-	end
 
 end
 
+function textbox:delete()
+	local string = textbox.text[textbox.selectedBox]
+
+	if (#string > 0) then
+		string = string:sub( 1, #string - 1 )
+		textbox.text[textbox.selectedBox] = string;
+	end
+end
 
 
 
@@ -60,41 +66,66 @@ function textbox:draw()
 	i = 0;
 	while (i <= self.boxCount) do
 		if (self.visibility[i] == true) then
-			love.graphics.rectangle("line",self.boxX[i], self.boxY[i], self.boxW[i], self.boxH[i])
-			love.graphics.print(self.text[i], self.boxX[i], self.boxY[i])
+
+			if self.style[i] == 0 then
+				love.graphics.rectangle("line",self.boxX[i], self.boxY[i], self.boxW[i], self.boxH[i])
+			elseif self.style[i] == 1 then
+				love.graphics.draw(textbox1, self.boxX[i], self.boxY[i])
+			elseif self.style[i] == 2 then
+				love.graphics.draw(textbox2, self.boxX[i], self.boxY[i])
+			end
+
+			if self.font[i] == 0 then
+				love.graphics.setFont(classyfont)
+			elseif self.font[i] == 1 then
+				love.graphics.setFont(defaultfont)
+			elseif self.font[i] == 2 then
+				love.graphics.setFont(coolfont)
+			end
+				love.graphics.printf(self.text[i], self.boxX[i]+self.boxhp[i], self.boxY[i]+self.boxvp[i], self.boxW[i]-2*self.boxhp[i], justify)
 		end
 		i=i+1;
 	end
 
-
-
 end
 
-
 function textbox.displayBox(boxCount,visible)
-	
+
 	if (visible == true) then
 		self.visibility[self.boxCount] = true;
-	elseif (visible == false) then
-		self.visibility[self.boxCount] = false;
+		elseif (visible == false) then
+			self.visibility[self.boxCount] = false;
 	end
 
 end
 
-function textbox:newBox(boxX,boxY,boxW,boxH,visible,text)
-	
-	self.boxX[self.boxCount] = boxX
+function textbox:newBox(boxX,boxY,boxW,boxH,visible,text,style,font,hp,vp)
+
+	if hp ~= nil then
+		self.boxhp[self.boxCount] = hp
+	else
+		self.boxhp[self.boxCount] = 0
+	end
+	if vp ~= nil then
+		self.boxvp[self.boxCount] = vp
+	else 
+		self.boxvp[self.boxCount] = 0
+	end
+
+	self.font[self.boxCount] = font
 	self.boxY[self.boxCount] = boxY
 	self.boxH[self.boxCount] = boxH
+	self.boxX[self.boxCount] = boxX
 	self.boxW[self.boxCount] = boxW
 	self.text[self.boxCount] = text
+	self.style[self.boxCount] = style
 
 	if (visible == 1) then
 		self.visibility[self.boxCount] = true;
-	elseif (visible == 0) then
-		self.visibility[self.boxCount] = false;
-	end
+		elseif (visible == 0) then
+			self.visibility[self.boxCount] = false;
+		end
 
-	self.boxCount = self.boxCount + 1;
+		self.boxCount = self.boxCount + 1;
 
 end
